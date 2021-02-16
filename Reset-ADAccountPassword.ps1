5349d90af8d92798cd0de2019c1ec38d29a7f920
Violet Haze Roś <code@unsola.ci>
2021-02-16 21:37:36 +0100

    feat: add -AsPlainText AND -Force parameters for specifying new password in plain text

--- Reset-ADAccountPassword.ps1
+++ Reset-ADAccountPassword.ps1
@@ -14,13 +14,19 @@
     Basically, a quick command line version of Active Directory Users and
     Computers'(dsa.msc) password reset dialog.
 .EXAMPLE
-    & .\Reset-ADAccountPassword.ps1 -Identity johndoe -NewPassword "P@$$w0rd" -Unlock -MustChangePasswordAtLogon
+    & .\Reset-ADAccountPassword.ps1 -Identity johndoe -NewPassword (Read-Host -Prompt "NewPassword" -AsSecureString) -Unlock -MustChangePasswordAtLogon
+.EXAMPLE
+    & .\Reset-ADAccountPassword.ps1 -Identity johndoe -NewPassword "P@$$w0rd" -AsPlainText -Force -MustChangePasswordAtLogon
 #>
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $true)][Microsoft.ActiveDirectory.Management.ADUser]$Identity,
     # Set new password for the account without providing old one
-    [Parameter(Mandatory = $true)][SecureString]$NewPassword,
+    $NewPassword,
+    # Specify a plain text password instead of secure string
+    [switch]$AsPlainText,
+    # Confirm that you understand the implications of using the AsPlainText parameter and still want to use it
+    [switch]$Force,
     # Unlock AD account
     [switch]$Unlock,
     # Set ChangePasswordAtLogon flag to $true
@@ -28,11 +34,36 @@ param (
 )
 
 $ADUser = Get-ADUser -Identity $Identity
+if ( ! $NewPassword ) {
+    if ($AsPlainText.IsPresent) {
+        Write-Verbose -Message "NewPassword not specified, ignoring AsPlainText parameter."
+    }
+    $NewPassword = Read-Host -Prompt "NewPassword" -AsSecureString
+}
 
-Set-ADAccountPassword `
+if ($NewPassword.GetType().Name -eq "SecureString") {
+    Set-ADAccountPassword `
     -Identity $ADUser `
     -Reset `
     -NewPassword $NewPassword
+} elseif ($NewPassword.GetType().Name -eq "String") {
+    if ($AsPlainText.IsPresent) {
+        if ($Force.IsPresent) {
+            Set-ADAccountPassword `
+                -Identity $ADUser `
+                -Reset `
+                -NewPassword ( `
+                    ConvertTo-SecureString `
+                        -AsPlainText `
+                        -Force `
+                        -String $NewPassword `
+                )
+        } else {
+            Write-Error `
+                -Message "The system cannot protect plain text input. To suppress this warning and convert the plain text to a SecureString, reissue the command specifying the Force parameter."
+        }
+    }
+}
 
 if ($Unlock.IsPresent) {
     Unlock-ADAccount -Identity $ADUser
